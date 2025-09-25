7346007ffe71e197f25f9a44f82e6ec82a7fc86f
NEXT71 <info@nextelbpo.co>
2025-09-26 02:04:25 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://www.nextelbpo.co',
+  origin: ['https://www.nextelbpo.co', 'https://nextelbpo.co'],
   credentials: true
 }));
 app.use(bodyParser.json());
